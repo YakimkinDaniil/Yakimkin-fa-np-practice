29fb86cc5ef020f0f9140538fc17bacd7bb67cc1
YakimkinDaniil <daniil666n@yandex.ru>
2024-03-28 11:36:05 +0300

    1

--- 1_echo_server/server.py
+++ 1_echo_server/server.py
@@ -1,7 +1,25 @@
 import socket
+import logging
+
+# Настройки логирования
+logging.basicConfig(
+    format="%(asctime)-15s [%(levelname)s] %(funcName)s: %(message)s",
+    handlers=[logging.FileHandler("server.log", 'w', 'utf-8'), logging.StreamHandler()],
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
 
 sock = socket.socket()
-sock.bind(('', 9090))
+
+port_input = input("Введите номер порта для сервера -> ")
+ip_input = input("Введите IP для сервера -> ")
+
+port = int(input('Введите омер порта для проверки ->'))
+
+password = str(input('Введите пароль - > '))
+
+
+sock.bind((ip_input, int(port_input)))
 sock.listen(0)
 conn, addr = sock.accept()
 print(addr)
@@ -9,11 +27,12 @@ print(addr)
 msg = ''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+ data = conn.recv(1024)
+ if not data:
+  break
+ msg += data.decode()
+ logging.info(f"Передача данных ")
+ conn.send(data)
 
 print(msg)
 
